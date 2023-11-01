9acf3ed5c3ab61c36966b61e18a9b6a13bb6871b
Angel S. Moreno <angelxmoreno@gmail.com>
2023-11-01 16:43:13 -0400

    feat: added lint run scripts with caching

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
     root: true,
-    extends: [
-        'universe/native',
+    extends: ['universe/native', 'universe/shared/typescript-analysis'],
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx', '*.d.ts'],
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+        },
     ],
 };
