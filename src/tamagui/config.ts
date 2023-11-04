3bc2f71713814406676be02434d4b4027e74bbf3
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2023-11-04 04:49:07 +0100

    feat: add custom theme config

--- src/tamagui/config.ts
+++ src/tamagui/config.ts
@@ -1,5 +1,7 @@
 import fonts from '@src/tamagui/fonts';
+import themes from '@src/tamagui/themes';
 import { config } from '@tamagui/config/v2-native';
+import { tokens } from '@tamagui/themes';
 import { createTamagui } from 'tamagui';
 
 const tamaguiConfig = createTamagui({
@@ -8,6 +10,8 @@ const tamaguiConfig = createTamagui({
         heading: fonts.headingFont,
         body: fonts.bodyFont,
     },
+    themes,
+    tokens,
 });
 
 export type AppConfig = typeof tamaguiConfig;
---
+++ src/tamagui/themes.ts
@@ -0,0 +1,89 @@
+import { createStrengthenMask, createThemeBuilder } from '@tamagui/theme-builder';
+
+const colorPalette_Light = [
+    '#ffffff',
+    '#ffeeed',
+    '#ffdcdc',
+    '#ffcbcb',
+    '#ffb9ba',
+    '#fda7a9',
+    '#fa9598',
+    '#f78288',
+    '#f36e77',
+    '#ee5967',
+    '#e94057',
+    '#000000',
+];
+
+const colorPalette_Active = ['#e94057', '#fff'];
+
+const colorPalette_Dark = [
+    '#000000',
+    '#030000',
+    '#130103',
+    '#290508',
+    '#410b12',
+    '#5a131c',
+    '#741b27',
+    '#902433',
+    '#ad2d3e',
+    '#cb364b',
+    '#e94057',
+    '#ffffff',
+];
+
+const themesBuilder = createThemeBuilder()
+    .addPalettes({
+        dark: colorPalette_Dark,
+        light: colorPalette_Light,
+        active: colorPalette_Active,
+    })
+    .addTemplates({
+        base: {
+            background: 0,
+            backgroundHover: 5,
+            backgroundPress: 6,
+            backgroundFocus: 2,
+            backgroundStrong: 4,
+            backgroundTransparent: -0,
+            color: -0,
+            colorHover: -2,
+            colorPress: -1,
+            colorFocus: -2,
+            colorTransparent: -0,
+            borderColor: -1,
+            borderColorHover: 6,
+            borderColorFocus: 4,
+            borderColorPress: 5,
+            placeholderColor: -1,
+            primary: -0,
+        },
+        active: {
+            background: 0,
+            color: 1,
+        },
+    })
+    .addMasks({
+        soften: createStrengthenMask(),
+    })
+    .addThemes({
+        light: {
+            template: 'base',
+            palette: 'light',
+        },
+        dark: {
+            template: 'base',
+            palette: 'dark',
+        },
+        active: {
+            template: 'active',
+            palette: 'active',
+        },
+    })
+    .addChildThemes({
+        subtle: {
+            mask: 'soften',
+        },
+    });
+
+export default themesBuilder.build();
