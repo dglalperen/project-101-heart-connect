5c809eb42349ae4e447cfc82721abf931f6e310e
Alperen Dagli <alperen.dagli43@gmail.com>
2023-11-11 00:49:29 +0300

    feat: Onboarding Screen

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Redirect } from 'expo-router';
 import React from 'react';
 
 function RootScreen() {
-    return <Redirect href="/signup" />;
+    return <Redirect href="/onboarding" />;
 }
 
 export default RootScreen;
