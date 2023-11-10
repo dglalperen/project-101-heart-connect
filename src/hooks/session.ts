ce83bac4f042bdf10635dd999e9f89ea3f5ca85d
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2023-11-10 19:13:10 +0100

    feat: add signIn and signUp methods to session hook

--- src/hooks/session.ts
+++ src/hooks/session.ts
@@ -4,6 +4,7 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 function useSession() {
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
     const [initialized, setInitialized] = useState<boolean>(false);
+
     const currentUser = useMemo<FirebaseAuthTypes.User | null>(
         () => auth().currentUser,
         [isLoggedIn],
@@ -26,13 +27,21 @@ function useSession() {
         [isLoggedIn, initialized],
     );
 
+    const signUp = useCallback(async (email: string, password: string) => {
+        return await auth().createUserWithEmailAndPassword(email, password);
+    }, []);
+
+    const signIn = useCallback(async (email: string, password: string) => {
+        return await auth().signInWithEmailAndPassword(email, password);
+    }, []);
+
     useEffect(() => {
         const subscriber = auth().onAuthStateChanged(e => onAuthStateChanged(e));
 
         return subscriber;
     }, [isLoggedIn]);
 
-    return { isLoggedIn, currentUser, initialized };
+    return { isLoggedIn, currentUser, initialized, signUp, signIn };
 }
 
 export default useSession;
